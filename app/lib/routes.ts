7e96a10bf1ef5fcb473daf5bc75b64b8461318d6
Angelo Dias <oiangelodias@gmail.com>
2025-04-30 18:34:53 -0300

    add routes and utils

---
+++ app/lib/routes.ts
@@ -0,0 +1,78 @@
+// PUBLIC
+const HOME = "/"
+const LOGIN = "/entrar"
+const FORGOT_PASSWORD = `${LOGIN}/esqueci`
+const LOGON = "/registrar"
+const LOGON_CALLBACK = `${LOGON}/callback`
+const LOGON_CONFIRM = `${LOGON}/confirm`
+
+////////
+// PRIVATE
+////////
+const LOGGED_AREA = "/dentro"
+
+// ACCOUNT
+const ACCOUNT = `${LOGGED_AREA}/conta`
+const RESET_PASSWORD = `${ACCOUNT}/resetar-senha`
+const BASIC_DATA = `${ACCOUNT}/dados-basicos`
+const CREATE_BASIC_DATA = `${BASIC_DATA}/criar`
+const EDIT_BASIC_DATA = `${BASIC_DATA}`
+
+// ADMIN
+const ADMIN_DASHBOARD = `${LOGGED_AREA}/admin`
+// ADMIN_EVENTS
+const ADMIN_EVENTS = `${ADMIN_DASHBOARD}/eventos`
+const ADMIN_EVENT_VIEW = (id: string) => `${ADMIN_EVENTS}/${id}`
+const ADMIN_EVENT_CREATE = `${ADMIN_EVENTS}/novo`
+const ADMIN_EVENT_EDIT = (id: string) => `${ADMIN_EVENT_VIEW(id)}/editar`
+
+// USER
+const DASHBOARD = `${LOGGED_AREA}`
+// PROFILE
+const AGREE_TO_TERMS = `${DASHBOARD}/termos`
+// EVENTS
+const EVENTS = `${DASHBOARD}/eventos` // Rota que redireciona
+const EVENT_VIEW = (id: string) => `${EVENTS}/${id}`
+const EVENT_RULES = (id: string) => `${EVENT_VIEW(id)}/regras`
+
+const routes = {
+  root: {
+    HOME,
+  },
+  auth: {
+    LOGIN,
+    FORGOT_PASSWORD,
+    LOGON,
+    LOGON_CALLBACK,
+    LOGON_CONFIRM,
+  },
+  dash: {
+    ROOT: LOGGED_AREA,
+    admin: {
+      ADMIN_DASHBOARD,
+      events: {
+        ADMIN_EVENTS,
+        ADMIN_EVENT_VIEW,
+        ADMIN_EVENT_CREATE,
+        ADMIN_EVENT_EDIT,
+      },
+    },
+    account: {
+      ACCOUNT,
+      RESET_PASSWORD,
+      basicData: {
+        CREATE: CREATE_BASIC_DATA,
+        EDIT: EDIT_BASIC_DATA,
+      },
+    },
+    participant: {
+      DASHBOARD,
+      AGREE_TO_TERMS,
+      events: {
+        EVENT_RULES,
+      },
+    },
+  },
+}
+
+export default routes
