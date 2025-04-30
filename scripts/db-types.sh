93c5766072eb48c40256371f78d4452c5e29efef
Angelo Dias <oiangelodias@gmail.com>
2025-04-30 18:34:55 -0300

    add script

---
+++ scripts/db-types.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+# Don't forget to chmod +x this file :)
+
+source .env
+
+echo "Generating Types from Supabase"
+
+# Default command
+SUPABASE_COMMAND="pnpx supabase gen types typescript --project-id $SUPABASE_PROJECT_ID --schema public"
+
+# Check if the --local flag is provided
+if [ "$1" = "--local" ]; then
+  SUPABASE_COMMAND="pnpx supabase gen types typescript --local --schema public"
+fi
+
+# Execute the chosen command and redirect output
+$SUPABASE_COMMAND > types/database.types.ts
+
+echo "Supabase types generation complete."
