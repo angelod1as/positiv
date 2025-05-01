4c483525fdfdb0a17ceda488fffb6d0c5f321d8b
Angelo Dias <oiangelodias@gmail.com>
2025-05-01 12:14:24 -0300

    add stupid debug page because of chrome web tools

--- app/routes.ts
+++ app/routes.ts
@@ -8,4 +8,10 @@ import {
 export default [
   index("pages/homepage/homepage.tsx"),
   layout("pages/auth/layout.tsx", [route("/entrar", "pages/auth/login.tsx")]),
+
+  // Debug route for Chrome DevTools
+  route(
+    "/.well-known/appspecific/com.chrome.devtools.json",
+    "pages/debug-null/debug-null.tsx",
+  ),
 ] satisfies RouteConfig
