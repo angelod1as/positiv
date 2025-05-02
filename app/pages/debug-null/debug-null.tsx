f60f28e0e0ebe2979377d434c7cba700701a3c75
Angelo Dias <oiangelodias@gmail.com>
2025-05-01 21:22:39 -0300

    improve debug null

--- app/pages/debug-null/debug-null.tsx
+++
@@ -1,7 +0,0 @@
-/**
- * This component exists because of a Chrome DevTools error with RR7
- * https://github.com/remix-run/react-router/issues/13516
- */
-export default function DebugNull() {
-  return null
-}
