6084cd2fd99ded4187c0bc58a8e378e600f75889
Angelo Dias <oiangelodias@gmail.com>
2025-05-03 20:45:48 -0300

    add centered layout

---
+++ app/components/layouts/centered-layout.tsx
@@ -0,0 +1,10 @@
+import type { FCC } from "~types/utils.types"
+
+export const CenteredLayout: FCC = (props) => {
+  return (
+    <div
+      {...props}
+      className="px-4 lg:px-0 self-center flex flex-col h-full max-w-2xl w-full gap-8 mb-12 mx-4 py-8"
+    />
+  )
+}
