4c2735cce4e5d20920a43e8d98e5152c1a63f3df
Angelo Dias <oiangelodias@gmail.com>
2025-05-05 20:56:50 -0300

    add mock POMs

---
+++ e2e/poms/account/change-password.pom.ts
@@ -0,0 +1,40 @@
+import { expect, type Locator, type Page } from "@playwright/test"
+import paths from "~/lib/paths"
+
+// TODO: THIS IS A MOCK PAGE
+export class ChangePasswordPOM {
+  readonly page: Page
+  readonly editBasicDataButton: Locator
+  readonly fillBasicDataButton: Locator
+  readonly changePasswordButton: Locator
+  readonly logoutButton: Locator
+  readonly deleteAccountButton: Locator
+
+  constructor(page: Page) {
+    this.page = page
+    // TODO: correct locators
+    this.editBasicDataButton = page.getByRole("link", { name: "" })
+    this.fillBasicDataButton = page.getByRole("link", { name: "" })
+    this.changePasswordButton = page.getByRole("link", { name: "" })
+    this.logoutButton = page.getByRole("link", { name: "" })
+    this.deleteAccountButton = page.getByRole("link", { name: "" })
+  }
+
+  async goto() {
+    await this.page.goto(paths.dash.account.ACCOUNT)
+  }
+
+  async testBasicElements(options?: { filledData: boolean }) {
+    const { filledData = true } = options || {}
+
+    if (filledData) {
+      await expect(this.editBasicDataButton).toBeVisible()
+    } else {
+      await expect(this.fillBasicDataButton).toBeVisible()
+    }
+
+    await expect(this.changePasswordButton).toBeVisible()
+    await expect(this.logoutButton).toBeVisible()
+    await expect(this.deleteAccountButton).toBeVisible()
+  }
+}
