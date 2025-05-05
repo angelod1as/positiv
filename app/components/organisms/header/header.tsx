c6cfa9d7e8cd2e7b3718d6b30520b7040f2671b3
Angelo Dias <oiangelodias@gmail.com>
2025-05-05 18:28:03 -0300

    add header

--- app/components/organisms/header/header.tsx
+++ app/components/organisms/header/header.tsx
@@ -1,10 +1,34 @@
+import { HomeIcon, UserIcon } from "lucide-react"
+import type { FC } from "react"
+import { useLoaderData } from "react-router"
 import PositivLogo from "~/assets/brand/positiv-logo-colors.png"
+import type { ProfileWithRoles } from "~/business/auth.server"
+import { Button } from "~/components/atoms/button/button"
 import { Link } from "~/components/atoms/link/link"
 import paths from "~/lib/paths"
 
-const { HOME } = paths.root
+const {
+  root: { HOME },
+  auth: { LOGIN },
+  dash: {
+    DASHBOARD,
+    account: { ACCOUNT },
+  },
+} = paths
 
-export const Header = () => {
+type LoaderData = {
+  profile:
+    | Pick<ProfileWithRoles, "social_name" | "full_name" | "email">
+    | undefined
+}
+
+export const Header: FC = () => {
+  const { profile } = useLoaderData<LoaderData>()
+
+  const displayName = profile
+    ? profile.social_name || profile.full_name || profile.email
+    : undefined
+
   return (
     <header className="flex items-center justify-between p-4 sticky top-0 left-0 z-30 w-full border-b bg-background/95 backdrop-blur-sm supports-backdrop-filter:bg-background/60 px-[1.75rem]">
       <div className="text-xl font-bold">
@@ -17,8 +41,19 @@ export const Header = () => {
         </Link>
       </div>
       <div className="flex items-center space-x-2">
-        {/* TODO: After setting up login */}
-        {/* <HeaderAccountData /> */}
+        {profile ? (
+          <div className="flex items-center space-x-2">
+            {!!displayName && <p>Olá, {displayName}</p>}
+            <Button asChild variant="outline" title="Dashboard" to={DASHBOARD}>
+              <HomeIcon />
+            </Button>
+            <Button asChild variant="outline" title="Conta" to={ACCOUNT}>
+              <UserIcon />
+            </Button>
+          </div>
+        ) : (
+          <Button to={LOGIN}>Login</Button>
+        )}
       </div>
     </header>
   )
