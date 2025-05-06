a4badf35b00a7caa646f922f3a31303a90cdd5ba
Angelo Dias <oiangelodias@gmail.com>
2025-05-06 08:25:07 -0300

    add event skeleton

--- app/pages/dashboard/dashboard-page.tsx
+++ app/pages/dashboard/dashboard-page.tsx
@@ -1,6 +1,9 @@
+import type { FC, ReactNode } from "react"
 import { redirect } from "react-router"
-import { getClientContext, getContext } from "~/business/auth/auth.server"
+import { getClientContext } from "~/business/auth/auth.client"
+import { getContext } from "~/business/auth/auth.server"
 import { EventCard } from "~/components/organisms/event-card/event-card"
+import { EventCardSkeleton } from "~/components/organisms/event-card/event-card-skeleton"
 import paths from "~/lib/paths"
 import type { ViewEvent } from "~types/entities.types"
 import { getNextEvents } from "../homepage/fetch/get-next-events"
@@ -36,6 +39,11 @@ export async function loader({ request, params }: Route.LoaderArgs) {
   if (!currentProfile?.basic_data_filled) {
     return redirect(AGREE_TO_TERMS, { headers: supabaseHeaders })
   }
+
+  return {
+    registrationOpen: [],
+    scheduled: [],
+  }
 }
 
 /* Needs to be clientLoader because getNextEvents needs new Date() */
@@ -53,47 +61,74 @@ export async function clientLoader({}: Route.LoaderArgs) {
   return splitEvents(events)
 }
 
-// TODO: HydrateFallback
+/* Force hydration on first load */
+clientLoader.hydrate = true as const
 
-const DashboardPage = ({ loaderData }: Route.ComponentProps) => {
-  const { registrationOpen, scheduled } = loaderData
-
+type WrapperProps = {
+  registrationEvents: ReactNode
+  scheduledEvents: ReactNode
+}
+const Wrapper: FC<WrapperProps> = ({ registrationEvents, scheduledEvents }) => {
   return (
     <div className="px-4 lg:px-8 pb-4 flex flex-col gap-12 ... mb-12 py-8">
       <div className="flex flex-col gap-4">
         <h2>Inscrições abertas</h2>
         <div className="grid gap-4 grid-cols-1 lg:grid-cols-2 2xl:grid-cols-3">
-          {registrationOpen.length ? (
-            registrationOpen.map((event) => (
-              <EventCard
-                data-testid="event-card-open"
-                key={event.id}
-                event={event}
-              />
-            ))
-          ) : (
-            <p>Nenhum evento com inscrições abertas</p>
-          )}
+          {registrationEvents}
         </div>
       </div>
 
       <div className="flex flex-col gap-4">
         <h2>Eventos agendados</h2>
         <div className="grid gap-4 grid-cols-1 lg:grid-cols-2 2xl:grid-cols-3">
-          {scheduled.length ? (
-            scheduled.map((event) => (
-              <EventCard
-                data-testid="event-card-scheduled"
-                key={event.id}
-                event={event}
-              />
-            ))
-          ) : (
-            <p>Nenhum evento encontrado</p>
-          )}
+          {scheduledEvents}
         </div>
       </div>
     </div>
+  )
+}
+
+export const HydrateFallback = () => {
+  return (
+    <Wrapper
+      registrationEvents={<EventCardSkeleton />}
+      scheduledEvents={<EventCardSkeleton />}
+    />
+  )
+}
+
+const DashboardPage = ({ loaderData }: Route.ComponentProps) => {
+  const { registrationOpen, scheduled } = loaderData
+
+  return (
+    <Wrapper
+      registrationEvents={
+        registrationOpen.length ? (
+          registrationOpen.map((event) => (
+            <EventCard
+              data-testid="event-card-open"
+              key={event.id}
+              event={event}
+            />
+          ))
+        ) : (
+          <p>Nenhum evento com inscrições abertas</p>
+        )
+      }
+      scheduledEvents={
+        scheduled.length ? (
+          scheduled.map((event) => (
+            <EventCard
+              data-testid="event-card-scheduled"
+              key={event.id}
+              event={event}
+            />
+          ))
+        ) : (
+          <p>Nenhum evento encontrado</p>
+        )
+      }
+    />
   )
 }
 
