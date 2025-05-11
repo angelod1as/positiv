98c79011cfda77796131c6ca1a04f2a36257d52f
Angelo Dias <oiangelodias@gmail.com>
2025-05-11 16:17:44 -0300

    add basic data pages

---
+++ app/pages/account/basic-data/gender-pronouns-orientation-page.tsx
@@ -0,0 +1,149 @@
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+import { Form, useSubmit } from "react-router"
+import { z } from "zod"
+import { getContext, getUserContext } from "~/business/auth/auth.server"
+import { genderPronounOrientationSchema } from "~/business/common"
+import { genderPronounsOrientation } from "~/business/participant/basic-data.server"
+import { CheckboxWithOther } from "~/components/forms/checkbox-with-other"
+import { Button } from "~/components/ui/button"
+import paths from "~/lib/paths"
+import type { Route } from "./+types/basic-data-page"
+
+const toOptions = (labels: string[]) =>
+  labels.map((label) => ({ label, value: label }))
+
+const {
+  dash: {
+    DASHBOARD,
+    account: { ACCOUNT },
+  },
+} = paths
+
+type FormData = z.infer<typeof genderPronounOrientationSchema>
+
+export async function action({ request, params }: Route.ActionArgs) {
+  const context = await getContext(request, params)
+  const formData = await request.formData()
+
+  const parsedData: FormData = {
+    gender: formData.get("gender")?.toString().split(",") || [],
+    orientation: formData.get("orientation")?.toString().split(",") || [],
+    pronouns: formData.get("pronouns")?.toString().split(",") || [],
+  }
+
+  return await genderPronounsOrientation({
+    formData: parsedData,
+    context,
+  })
+}
+
+export async function loader({ request, params }: Route.LoaderArgs) {
+  const { currentProfile: profile } = await getUserContext(request, params)
+  return { profile }
+}
+
+const otherFilter = (item: string) => item !== "other"
+
+const GenderPronounOrientationPage = ({ loaderData }: Route.ComponentProps) => {
+  const submit = useSubmit()
+  const { profile } = loaderData || {}
+
+  const {
+    control,
+    handleSubmit,
+    formState: { errors },
+    clearErrors,
+  } = useForm({
+    defaultValues: {
+      gender: profile?.gender || [],
+      orientation: profile?.orientation || [],
+      pronouns: profile?.pronouns || [],
+    },
+    reValidateMode: "onSubmit",
+    resolver: zodResolver(genderPronounOrientationSchema),
+    shouldFocusError: true,
+  })
+
+  const onSubmit = (val: FormData) => {
+    const cleaned: FormData = {
+      gender: val.gender.filter(otherFilter),
+      orientation: val.orientation.filter(otherFilter),
+      pronouns: val.pronouns.filter(otherFilter),
+    }
+    submit(cleaned, {
+      method: "POST",
+    })
+  }
+
+  const handleChange = () => {
+    clearErrors()
+  }
+
+  return (
+    <>
+      <div>
+        <h1>Dados básicos</h1>
+        <p className="text-muted-foreground">
+          {profile?.basic_data_filled
+            ? "Atualize seus dados"
+            : "Precisamos destes dados básicos para nosso controle interno de pessoas participantes"}
+        </p>
+      </div>
+      <Form onChange={handleChange} onSubmit={handleSubmit(onSubmit)}>
+        <div className="flex flex-col gap-6 sm:grid grid-cols-12 sm:gap-4">
+          <CheckboxWithOther
+            control={control}
+            errors={errors}
+            className="col-span-4"
+            name="gender"
+            options={toOptions([
+              "Mulher cis",
+              "Mulher trans",
+              "Travesti",
+              "Pessoa não binária",
+              "Pessoa agênera",
+              "Homem trans",
+              "Homem cis",
+            ])}
+          />
+          <CheckboxWithOther
+            control={control}
+            errors={errors}
+            className="col-span-4"
+            name="orientation"
+            options={toOptions([
+              "Mulher cis",
+              "Mulher trans",
+              "Travesti",
+              "Pessoa não binária",
+              "Pessoa agênera",
+              "Homem trans",
+              "Homem cis",
+            ])}
+          />
+          <CheckboxWithOther
+            control={control}
+            errors={errors}
+            className="col-span-4"
+            name="pronouns"
+            options={toOptions([
+              "Mulher cis",
+              "Mulher trans",
+              "Travesti",
+              "Pessoa não binária",
+              "Pessoa agênera",
+              "Homem trans",
+              "Homem cis",
+            ])}
+          />
+        </div>
+        <Button type="submit" className="mt-4">
+          Continuar
+        </Button>
+      </Form>
+    </>
+  )
+}
+
+export default GenderPronounOrientationPage
