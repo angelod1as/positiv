a565bcf9a6523c20c76aca93b15772756ddbdc57
Angelo Dias <oiangelodias@gmail.com>
2025-05-11 00:09:59 -0300

    update helpers and paths

---
+++ app/lib/helpers/make-typed-environment.ts
@@ -0,0 +1,21 @@
+import type { CamelKeys } from "string-ts"
+import { camelKeys } from "string-ts"
+
+// TODO: move helpers into one file with correct documentation. Example below.
+
+/**
+ * Creates a typed environment function that parses, camelCases, and caches the arguments.
+ * @param schema - The schema used to parse the arguments.
+ * @returns A function that takes arguments and returns a typed environment.
+ */
+export function makeTypedEnvironment<T>(schema: { parse: (u: unknown) => T }) {
+  let env: CamelKeys<T>
+  // args will usually be process.env but can be any object
+  return (args: Record<string, unknown>) => {
+    // if the env is already set, return it
+    if (env) return env
+    // if the env is not set, parse the args and camelCase them
+    env = camelKeys(schema.parse(args))
+    return env
+  }
+}
