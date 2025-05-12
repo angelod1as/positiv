e3dde53084af281fea7f3d568ef9aef53ebfa1ef
Angelo Dias <oiangelodias@gmail.com>
2025-05-12 19:18:01 -0300

    add mailhog test to setup

--- e2e/setup/global.setup.ts
+++ e2e/setup/global.setup.ts
@@ -6,7 +6,21 @@ import { BasicDataPOM } from "e2e/poms/account/basic-data.pom"
 import { GenderPronounOrientationPOM } from "e2e/poms/account/gender-pronouns-orientation.pom"
 import { LoginPOM } from "e2e/poms/auth/login.pom"
 import { AgreeToTermsPOM } from "e2e/poms/dashboard/agree-to-terms.pom"
+import { HomepagePOM } from "e2e/poms/homepage.pom"
+import { MailhogPOM } from "e2e/poms/mailhog/mailhog.pom"
 import { createMockUser } from "./create-mock-user"
+
+test("Check if Mailhog is running", async ({ page }) => {
+  pwLog("checking if Mailhog is running")
+
+  const homepage = new HomepagePOM(page)
+  await homepage.goto()
+  await homepage.testBasicElements()
+
+  const mailHogPage = new MailhogPOM(await page.context().newPage())
+  await mailHogPage.goto()
+  await mailHogPage.testBasicElements()
+})
 
 test("authenticate as participant", async ({ page }) => {
   pwLog("authenticating as participant")
