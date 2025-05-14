88c87ca42f5ec36a6186475a4a399ad6863ec989
Angelo Dias <oiangelodias@gmail.com>
2025-05-14 17:33:44 -0300

    remove isCI

--- app/lib/email/email.ts
+++ app/lib/email/email.ts
@@ -1,6 +1,5 @@
 import Mail from "nodemailer/lib/mailer"
 import { env } from "~/env.server"
-import { isCI } from "../helpers/is-prod.server"
 import { getEmailTransport } from "./get-email-transport"
 
 const fromEmail = env().fromEmail
@@ -13,8 +12,6 @@ export interface MailOptions extends Omit<Mail.Options, "from"> {
 }
 
 export const sendMail = async (mailOptions: MailOptions): Promise<void> => {
-  if (isCI()) return
-
   const transport = getEmailTransport()
 
   transport.sendMail(
