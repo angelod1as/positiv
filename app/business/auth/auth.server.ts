1bc5d10134bdc40f86f00639690336afde067e23
Angelo Dias <oiangelodias@gmail.com>
2025-05-14 22:19:56 -0300

    rollback loginUser

--- app/business/auth/auth.server.ts
+++ app/business/auth/auth.server.ts
@@ -15,6 +15,7 @@ import {
   currentUserSchema,
   forgotPasswordSchema,
   getSupabaseSchema,
+  loginSchema,
   registerUserSchema,
 } from "../common"
 
@@ -111,6 +112,25 @@ export const getUserContext = async (
   }
   return { ...context, currentUser }
 }
+
+export const loginUser = applySchema(
+  loginSchema,
+  contextSchema,
+)(async (values, context) => {
+  const { supabase } = context
+  const { error, data } = await supabase.auth.signInWithPassword(values)
+
+  if (error) {
+    if (error.code === "invalid_credentials") {
+      throw new Error("Credenciais inválidas")
+    }
+    throw new Error(
+      `Erro de autenticação — Código: "${error.code}" — Mensagem: "${error.message}"`,
+    )
+  }
+
+  return { user: data.user }
+})
 
 export const getUserCodeContext = async (request: Request, params: Params) => {
   const requestUrl = new URL(request.url)
