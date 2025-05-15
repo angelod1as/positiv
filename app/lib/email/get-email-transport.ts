a6eecdba72cf2cc84d6653ac0c827bce124e9011
Angelo Dias <oiangelodias@gmail.com>
2025-05-15 18:29:21 -0300

    update SES

--- app/lib/email/get-email-transport.ts
+++ app/lib/email/get-email-transport.ts
@@ -1,4 +1,4 @@
-import * as aws from "@aws-sdk/client-ses"
+import { SESv2Client, SendEmailCommand } from "@aws-sdk/client-sesv2"
 import { defaultProvider } from "@aws-sdk/credential-provider-node"
 import nodemailer from "nodemailer"
 import { isProd } from "../helpers/is-prod.server"
@@ -7,15 +7,14 @@ export function getEmailTransport() {
   const prod = isProd()
 
   if (prod) {
-    const ses = new aws.SES({
-      apiVersion: "2010-12-01",
+    const sesClient = new SESv2Client({
       region: "sa-east-1",
       credentials: defaultProvider(),
     })
 
     return nodemailer.createTransport({
-      SES: { ses, aws },
-      sendingRate: 1, // max 1 messages/second
+      SES: { sesClient, SendEmailCommand },
+      sendingRate: 1, // max 1 messages/second — dunno if it works
       maxConnections: 1,
     })
   }
