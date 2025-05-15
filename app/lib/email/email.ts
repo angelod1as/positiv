f2669fef92dcabe85fe59aa470ed961b2daea45a
Angelo Dias <oiangelodias@gmail.com>
2025-05-15 07:18:44 -0300

    fix email from

--- app/lib/email/email.ts
+++ app/lib/email/email.ts
@@ -1,9 +1,6 @@
-import pwLog from "e2e/helpers/log"
 import Mail from "nodemailer/lib/mailer"
-import { env } from "~/env.server"
+import { POSITIV_EMAIL } from "../helpers/constants"
 import { getEmailTransport } from "./get-email-transport"
-
-const fromEmail = env().fromEmail
 
 export interface MailOptions extends Omit<Mail.Options, "from"> {
   to: NonNullable<Mail.Options["to"]>
@@ -15,19 +12,9 @@ export interface MailOptions extends Omit<Mail.Options, "from"> {
 export const sendMail = async (mailOptions: MailOptions): Promise<void> => {
   const transport = getEmailTransport()
 
-  pwLog(
-    `\n\n:DEV mailOptions:\n`,
-    "fromEmail",
-    fromEmail,
-    "\n",
-    "mailOptions",
-    mailOptions.toString(),
-    `\n\n`,
-  )
-
   transport.sendMail(
     {
-      from: fromEmail,
+      from: POSITIV_EMAIL,
       ...mailOptions,
     },
     (
@@ -36,7 +23,6 @@ export const sendMail = async (mailOptions: MailOptions): Promise<void> => {
     ) => {
       if (err) {
         console.error("transport.sendMail error")
-        console.error("fromEmail", fromEmail)
         console.error("mailOptions", mailOptions)
         console.error(err)
         return
