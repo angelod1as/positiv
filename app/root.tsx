f584a7523574f4a623de1b326e3e5cb73b9c7e6c
Angelo Dias <oiangelodias@gmail.com>
2025-05-15 22:10:50 -0300

    add meta

--- app/root.tsx
+++ app/root.tsx
@@ -33,6 +33,39 @@ export const links: Route.LinksFunction = () => [
   },
 ]
 
+export function meta({}: Route.MetaArgs) {
+  return [
+    { title: "Positiv Party" },
+    {
+      name: "description",
+      content:
+        "Eventos para amantes de saliências não-mono, curioses com o mundo da suruba, e quem quer explorar a própria sexualidade",
+    },
+    {
+      rel: "apple-touch-icon",
+      sizes: "180x180",
+      href: "/apple-touch-icon.png",
+    },
+    {
+      rel: "icon",
+      type: "image/png",
+      sizes: "32x32",
+      href: "/favicon-32x32.png",
+    },
+    {
+      rel: "icon",
+      type: "image/png",
+      sizes: "16x16",
+      href: "/favicon-16x16.png",
+    },
+    {
+      rel: "manifest",
+      sizes: "180x180",
+      href: "/site.webmanifest",
+    },
+  ]
+}
+
 export async function loader({ params, request }: Route.LoaderArgs) {
   const { currentProfile, isProd } = await getContext(request, params)
   const { toast, headers } = await getToast(request)
@@ -46,6 +79,7 @@ export function Layout(props: { children: ReactNode }) {
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+
         <Meta />
         <Links />
       </head>
