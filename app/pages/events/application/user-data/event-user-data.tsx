774ad26168b2d69f5885dcd050cba59b36e9b215
Angelo Dias <oiangelodias@gmail.com>
2025-05-22 08:08:23 -0300

    add cookie session server

--- app/pages/events/application/user-data/event-user-data.tsx
+++ app/pages/events/application/user-data/event-user-data.tsx
@@ -1,5 +1,20 @@
+import { redirect } from "react-router"
 import { applyToEvent } from "~/business/participant/apply-to-event.server"
+import { getSession } from "~/business/session.server"
+import paths from "~/lib/paths"
 import type { Route } from "./+types/event-user-data"
+
+const {
+  dash: {
+    events: { EVENT_RULES },
+  },
+} = paths
+
+export async function loader({ request, params }: Route.LoaderArgs) {
+  const session = await getSession(request.headers.get("Cookie"))
+  const isRulesCorrect = session.get("rulesCorrect")
+  if (!isRulesCorrect) return redirect(EVENT_RULES(params.id))
+}
 
 export async function action({ request, params }: Route.ActionArgs) {
   return await applyToEvent(request, params)
