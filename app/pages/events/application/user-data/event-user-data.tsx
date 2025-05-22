ab26bee8f2b8bafdd77ea8428588f13be81c96c0
Angelo Dias <oiangelodias@gmail.com>
2025-05-22 17:50:26 -0300

    update cookie

--- app/pages/events/application/user-data/event-user-data.tsx
+++ app/pages/events/application/user-data/event-user-data.tsx
@@ -1,6 +1,8 @@
 import { redirect } from "react-router"
+import { applyToEventSchema } from "~/business/common"
 import { applyToEvent } from "~/business/participant/apply-to-event.server"
-import { getSession } from "~/business/session.server"
+import { rulesSessionStorage } from "~/business/session.server"
+import { SchemaForm } from "~/components/forms/schema-form"
 import paths from "~/lib/paths"
 import type { Route } from "./+types/event-user-data"
 
@@ -11,6 +13,7 @@ const {
 } = paths
 
 export async function loader({ request, params }: Route.LoaderArgs) {
+  const { getSession } = rulesSessionStorage
   const session = await getSession(request.headers.get("Cookie"))
   const isRulesCorrect = session.get("rulesCorrect")
   if (!isRulesCorrect) return redirect(EVENT_RULES(params.id))
@@ -21,7 +24,35 @@ export async function action({ request, params }: Route.ActionArgs) {
 }
 
 const EventUserInfo = () => {
-  return <div>EventUserInfo</div>
+  return (
+    <>
+      <h1>Quase lá!</h1>
+      <p>
+        Parabéns, você acertou tudo! Essa é a última etapa: precisamos de
+        algumas informações específicas à inscrição nesse evento.
+      </p>
+      <p>
+        Ao clicar no botão "Confirmar Inscrição", sua inscrição será confirmada
+        (óbvio) e você irá receber um email com os dados do evento, salve na sua
+        agenda!
+      </p>
+      <p>(O email pode demorar uns minutos para chegar)</p>
+
+      <SchemaForm
+        schema={applyToEventSchema}
+        hiddenFields={["applicationDate"]}
+        values={{
+          applicationDate: new Date(),
+        }}
+        multiline={["notes"]}
+        labels={{
+          notes:
+            "Se você tiver alguma nota ou comentário que gostaria que as pessoas administradoras soubessem, escreva-as abaixo:",
+        }}
+        buttonLabel="🎉 Confirmar Inscrição!"
+      />
+    </>
+  )
 }
 
 export default EventUserInfo
