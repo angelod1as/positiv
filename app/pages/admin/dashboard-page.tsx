56159cacc083f362a262c51e82bcd8642fa1789f
Angelo Dias <oiangelodias@gmail.com>
2025-05-29 11:35:39 -0300

    WIP move event to dashboard

--- app/pages/admin/dashboard-page.tsx
+++ app/pages/admin/dashboard-page.tsx
@@ -1,20 +1,39 @@
-import { Button } from "~/components/atoms/button/button"
-import paths from "~/lib/paths"
+import { getAdminContext } from "~/business/admin/admin.server"
+import { Separator } from "~/components/ui/separator"
 import type { Route } from "./+types/dashboard-page"
+import AdminDashboardEventsTable from "./admin-dashboard-events-table"
 
-const {
-  admin: {
-    events: { ADMIN_EVENTS },
-  },
-} = paths
+export async function loader({ request, params }: Route.LoaderArgs) {
+  const { events } = await getAdminContext(request, params)
+  if (!events) return { events: undefined }
 
-const AdminDashboard = ({}: Route.ComponentProps) => {
+  const sorted = events.sort((a, b) => {
+    const startA = a.time_event_start
+    const startB = b.time_event_start
+    if (!startA || !startB) {
+      return -1
+    }
+
+    return new Date(startA).getTime() - new Date(startB).getTime()
+  })
+  return { events: sorted }
+}
+
+const AdminDashboard = ({ loaderData }: Route.ComponentProps) => {
+  const { events } = loaderData
+
   return (
     <>
-      <Button to={ADMIN_EVENTS}>Ver eventos</Button>
-      <Button to="" disabled>
-        Ver participantes (em breve)
-      </Button>
+      <h1>Visão geral</h1>
+      <div>
+        <h2>Eventos</h2>
+
+        <AdminDashboardEventsTable events={events} />
+      </div>
+      <Separator />
+      <div>
+        <h2>Participantes (em breve)</h2>
+      </div>
     </>
   )
 }
