62a25e75c66d94b5fa6f09c43112e5e1521306b6
Angelo Dias <oiangelodias@gmail.com>
2025-06-03 16:51:36 -0300

    update table

--- app/pages/admin/events-table/admin-dashboard-events-table.tsx
+++ app/pages/admin/events-table/admin-dashboard-events-table.tsx
@@ -1,7 +1,9 @@
 import { EyeIcon, PencilIcon, UsersIcon } from "lucide-react"
+import { FilterMatchMode } from "primereact/api"
 import { Column } from "primereact/column"
-import { DataTable } from "primereact/datatable"
-import { useState, type FC } from "react"
+import { DataTable, type DataTableFilterMeta } from "primereact/datatable"
+import { InputText } from "primereact/inputtext"
+import { useState, type ChangeEvent, type FC } from "react"
 import { Button } from "~/components/atoms/button/button"
 import { formatDateTime } from "~/lib/helpers/format-date-time"
 import paths from "~/lib/paths"
@@ -16,81 +18,99 @@ const {
 type AdminDashboardEventsTableProps = {
   events: Event[]
 }
-
-const columns: Array<{
-  field: keyof Event
-  header: string
-  transform?: (value: Event) => string
-}> = [
-  { field: "id", header: "Id" },
-  { field: "title", header: "Title" },
-  {
-    field: "time_event_start",
-    header: "Data de início",
-    transform: (value) => {
-      return formatDateTime(value.time_event_start).full || ""
-    },
-  },
-]
-
-const buttons = (value: Event) => {
-  const eventId = value.id
-  return (
-    <div className="flex gap-2 justify-self-end">
-      <Button to={ADMIN_EVENT_PARTICIPANTS(eventId)} variant="outline">
-        <UsersIcon />
-      </Button>
-      <Button to={ADMIN_VIEW_EVENT(eventId)} variant="outline">
-        <EyeIcon />
-      </Button>
-      <Button to={ADMIN_EDIT_EVENT(eventId)} variant="outline">
-        <PencilIcon />
-      </Button>
-    </div>
-  )
-}
-
 export const AdminDashboardEventsTable: FC<AdminDashboardEventsTableProps> = ({
   events,
 }) => {
   const [selection, setSelection] = useState<Event[]>([])
-  return (
-    <div className="z-50 ">
-      <DataTable<Event[]>
-        value={events}
-        scrollable
-        scrollHeight="400px"
-        stripedRows
-        paginator
-        rows={150}
-        tableStyle={{ minWidth: "50rem" }}
-        sortMode="multiple"
-        removableSort
-        sortField="time_event_start"
-        sortOrder={-1}
-        selection={selection}
-        onSelectionChange={(e) => setSelection(e.value)}
-        selectionMode="checkbox"
-        dataKey="id"
-        resizableColumns
-        columnResizeMode="fit"
-        stateStorage="session"
-        stateKey="dt-state-admin-events"
-        emptyMessage="Nenhum evento encontrado"
-      >
-        <Column selectionMode="multiple" headerStyle={{ width: "3rem" }} />
 
-        {columns.map((col) => (
-          <Column
-            key={col.field}
-            field={col.field}
-            header={col.header}
-            sortable
-            body={col.transform ? col.transform : undefined}
-          />
-        ))}
-        <Column body={buttons} />
-      </DataTable>
-    </div>
+  const [filters, setFilters] = useState<DataTableFilterMeta>({
+    global: { value: null, matchMode: FilterMatchMode.CONTAINS },
+  })
+
+  const [globalFilterValue, setGlobalFilterValue] = useState("")
+
+  const onGlobalFilterChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const value = e.target.value
+    const stateFilters = { ...filters }
+
+    if ("value" in stateFilters["global"]) {
+      stateFilters["global"].value = value
+      setFilters(stateFilters)
+      setGlobalFilterValue(value)
+    }
+  }
+
+  const renderHeader = () => {
+    return (
+      <div className="flex justify-content-end">
+        <InputText
+          value={globalFilterValue}
+          onChange={onGlobalFilterChange}
+          placeholder="Buscar..."
+        />
+      </div>
+    )
+  }
+
+  return (
+    <DataTable
+      value={events}
+      paginator
+      rows={150}
+      dataKey="id"
+      emptyMessage="Nenhum evento encontrado"
+      // Filters
+      filters={filters}
+      filterDisplay="row"
+      onFilter={(e) => setFilters(e.filters)}
+      header={renderHeader}
+      // Session
+      // TODO: After fixing
+      // stateStorage="session"
+      // stateKey="dt-state-admin-events"
+      // Scroll
+      scrollable
+      scrollHeight="400"
+      stripedRows
+      // Sorting
+      sortField="time_event_start"
+      sortOrder={-1}
+      // Selection
+      selection={selection}
+      onSelectionChange={(e) => setSelection(e.value)}
+      selectionMode="checkbox"
+      // Resize
+      resizableColumns
+      columnResizeMode="fit"
+    >
+      <Column selectionMode="multiple" headerStyle={{ width: "3rem" }} />
+      <Column field="id" header="id" hidden={true} />
+      <Column field="title" header="Título" alignFrozen="left" frozen={true} />
+      <Column
+        field="time_event_start"
+        header="Data de início"
+        body={(value) => formatDateTime(value.time_event_start).full}
+      />
+
+      {/* Buttons */}
+      <Column
+        body={(value: Event) => {
+          const eventId = value.id
+          return (
+            <div className="flex gap-2 justify-self-end">
+              <Button to={ADMIN_EVENT_PARTICIPANTS(eventId)} variant="outline">
+                <UsersIcon />
+              </Button>
+              <Button to={ADMIN_VIEW_EVENT(eventId)} variant="outline">
+                <EyeIcon />
+              </Button>
+              <Button to={ADMIN_EDIT_EVENT(eventId)} variant="outline">
+                <PencilIcon />
+              </Button>
+            </div>
+          )
+        }}
+      />
+    </DataTable>
   )
 }
