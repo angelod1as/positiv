c974c1f31d5a1e09c201e3a2557905a144a57195
Angelo Dias <oiangelodias@gmail.com>
2025-06-04 22:17:42 -0300

    add events with registration closed to the dashboard

--- app/components/organisms/event-card/event-card.tsx
+++ app/components/organisms/event-card/event-card.tsx
@@ -1,9 +1,5 @@
-import { DialogClose } from "@radix-ui/react-dialog"
 import type { FC } from "react"
-import { useFetcher } from "react-router"
-import { Button } from "~/components/atoms/button/button"
 import { DataPair } from "~/components/atoms/data-pair/data-pair"
-import ConfirmDialog from "~/components/molecules/confirm-dialog/confirm-dialog"
 import {
   Card,
   CardContent,
@@ -12,42 +8,19 @@ import {
   CardHeader,
   CardTitle,
 } from "~/components/ui/card"
-import {
-  Dialog,
-  DialogContent,
-  DialogFooter,
-  DialogHeader,
-  DialogTitle,
-  DialogTrigger,
-} from "~/components/ui/dialog"
+
 import { formatDateTime } from "~/lib/helpers/format-date-time"
 import { generateGoogleCalendarLink } from "~/lib/helpers/generate-google-calendar-link"
-import paths from "~/lib/paths"
 import type { ViewEvent } from "~types/entities.types"
 import { DateStatus } from "./date-status"
-
-const {
-  dash: {
-    participant: { DOWNLOAD_CALENDAR },
-    events: { EVENT_VIEW },
-  },
-} = paths
+import { EventCardFooter } from "./event-card-footer"
 
 type EventCardProps = { event: ViewEvent; "data-testid": string }
 export const EventCard: FC<EventCardProps> = ({
   event,
   "data-testid": dataTestId,
 }) => {
-  const fetcher = useFetcher()
-
-  /* Handle Cancel Application */
-  const handleConfirm = async (closeDialog: () => void) => {
-    await fetcher.submit(
-      { cancel: true, eventId: event.id },
-      { method: "POST" },
-    )
-    closeDialog()
-  }
+  const googleLink = generateGoogleCalendarLink(event)
 
   const {
     time_application_end,
@@ -69,9 +42,6 @@ export const EventCard: FC<EventCardProps> = ({
     title,
     is_applied,
   } = event
-
-  const isEventOpen = event_status === "Registration Open"
-  const googleLink = generateGoogleCalendarLink(event)
 
   return (
     <Card className="flex flex-col flex-1" data-testid={dataTestId}>
@@ -114,69 +84,13 @@ export const EventCard: FC<EventCardProps> = ({
         </div>
       </CardContent>
       <CardFooter>
-        {is_applied ? (
-          <div className="flex flex-col sm:flex-row gap-4 w-full">
-            <Dialog>
-              <DialogTrigger asChild>
-                <Button variant="outline">Adicionar ao Calendário</Button>
-              </DialogTrigger>
-              <DialogContent className="max-w-sm sm:max-w-sm">
-                <DialogHeader>
-                  <DialogTitle>Adicionar ao calendário</DialogTitle>
-                </DialogHeader>
-                <DialogFooter className="flex flex-col sm:justify-between">
-                  {googleLink && (
-                    <DialogClose asChild>
-                      <Button
-                        variant="outline"
-                        to={googleLink}
-                        linkProps={{ target: "_blank" }}
-                      >
-                        Google Calendar
-                      </Button>
-                    </DialogClose>
-                  )}
-                  <DialogClose asChild>
-                    <Button
-                      variant="outline"
-                      to={DOWNLOAD_CALENDAR(event.id)}
-                      linkProps={{ reloadDocument: true }}
-                    >
-                      Baixar arquivo iCal
-                    </Button>
-                  </DialogClose>
-                </DialogFooter>
-              </DialogContent>
-            </Dialog>
-
-            <fetcher.Form method="post">
-              <ConfirmDialog
-                title="Cancelar inscrição"
-                description={
-                  <div>
-                    <p>Você tem certeza que deseja cancelar sua inscrição?</p>
-                  </div>
-                }
-                confirmLabel="😢 Cancelar"
-                cancelLabel="🎉 Voltar"
-                isLoading={fetcher.state !== "idle"}
-                onConfirm={handleConfirm}
-              >
-                <ConfirmDialog.Trigger variant="destructive" className="w-full">
-                  Cancelar inscrição
-                </ConfirmDialog.Trigger>
-              </ConfirmDialog>
-            </fetcher.Form>
-          </div>
-        ) : (
-          <Button
-            data-testid={dataTestId}
-            to={isEventOpen ? EVENT_VIEW(id) : ""}
-            disabled={!isEventOpen}
-          >
-            {isEventOpen ? "Fazer inscrição" : "Inscreva-se em breve"}
-          </Button>
-        )}
+        <EventCardFooter
+          eventId={id}
+          event_status={event_status}
+          googleLink={googleLink}
+          is_applied={is_applied}
+          dataTestId={dataTestId}
+        />
       </CardFooter>
     </Card>
   )
