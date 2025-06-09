775ac79c2515c6ed322c40903e36f2d4c702aa0b
Angelo Dias <oiangelodias@gmail.com>
2025-06-09 18:44:36 -0300

    add filtering

--- app/pages/admin/events-table/admin-dashboard-events-table.tsx
+++ app/pages/admin/events-table/admin-dashboard-events-table.tsx
@@ -1,14 +1,18 @@
 import { EyeIcon, PencilIcon, UsersIcon } from "lucide-react"
 import { FilterMatchMode, FilterService } from "primereact/api"
-import { Column } from "primereact/column"
+import {
+  Column,
+  type ColumnFilterElementTemplateOptions,
+} from "primereact/column"
 import { DataTable, type DataTableFilterMeta } from "primereact/datatable"
+import { Dropdown } from "primereact/dropdown"
 import { InputText } from "primereact/inputtext"
 import { useState, type ChangeEvent, type FC } from "react"
 import { Button } from "~/components/atoms/button/button"
 import { formatDateTime } from "~/lib/helpers/format-date-time"
-import { eventPropNameMap } from "~/lib/helpers/propMaps"
+import { eventPropNameMap, eventStatusMap } from "~/lib/helpers/propMaps"
 import paths from "~/lib/paths"
-import type { Event } from "~types/entities.types"
+import type { Event, EventStatus } from "~types/entities.types"
 
 const {
   admin: {
@@ -38,6 +42,7 @@ export const AdminDashboardEventsTable: FC<AdminDashboardEventsTableProps> = ({
   const [filters, setFilters] = useState<DataTableFilterMeta>({
     global: { value: null, matchMode: FilterMatchMode.CONTAINS },
     time_event_start: { value: null, matchMode: FilterMatchMode.CUSTOM },
+    event_status: { value: null, matchMode: FilterMatchMode.EQUALS },
   })
 
   const [globalFilterValue, setGlobalFilterValue] = useState("")
@@ -78,6 +83,31 @@ export const AdminDashboardEventsTable: FC<AdminDashboardEventsTableProps> = ({
   //   )
   // }
 
+  const statusFilterTemplate = (
+    options: ColumnFilterElementTemplateOptions,
+  ) => {
+    return (
+      <Dropdown
+        value={options.value}
+        options={[
+          "Draft",
+          "Scheduled",
+          "Registration Open",
+          "Registration Closed",
+          "Cancelled",
+          "Completed",
+        ].map((status) => ({
+          label: eventStatusMap(status as EventStatus),
+          value: status,
+        }))}
+        onChange={(e) => options.filterCallback(e.value, options.index)}
+        placeholder="Selecione"
+        className="p-column-filter"
+        showClear
+      />
+    )
+  }
+
   return (
     <DataTable
       value={events}
@@ -87,7 +117,7 @@ export const AdminDashboardEventsTable: FC<AdminDashboardEventsTableProps> = ({
       emptyMessage="Nenhum evento encontrado"
       // Filters
       filters={filters}
-      // filterDisplay="menu"
+      filterDisplay="menu"
       onFilter={(e) => setFilters(e.filters)}
       globalFilterFields={["title"]}
       header={renderHeader}
@@ -101,7 +131,7 @@ export const AdminDashboardEventsTable: FC<AdminDashboardEventsTableProps> = ({
       stripedRows
       // Sorting
       sortField="time_event_start"
-      sortOrder={-1}
+      sortOrder={1}
       removableSort
       // Selection
       selection={selection}
@@ -126,6 +156,14 @@ export const AdminDashboardEventsTable: FC<AdminDashboardEventsTableProps> = ({
         alignFrozen="left"
         sortable
         frozen={true}
+      />
+      <Column
+        field="event_status"
+        header={eventPropNameMap("event_status")}
+        body={(value) => eventStatusMap(value.event_status)}
+        filter
+        filterElement={statusFilterTemplate}
+        showFilterMatchModes={false}
       />
       <Column
         field="time_event_start"
