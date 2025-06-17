c5dcffaabec407744a7b61b30239d03eb5207e0e
Angelo Dias <oiangelodias@gmail.com>
2025-06-17 13:30:45 -0300

    add missing info in TOC

--- app/components/layouts/centered-layout.tsx
+++ app/components/layouts/centered-layout.tsx
@@ -4,7 +4,7 @@ export const CenteredLayout: FCC = (props) => {
   return (
     <div
       {...props}
-      className="px-4 lg:px-0 self-center flex flex-col h-full max-w-2xl w-full gap-8 mb-12 mx-4 py-8"
+      className="px-4 lg:px-0 self-center flex flex-col h-full max-w-2xl w-full gap-8 mb-12 mx-4 py-8 centered-layout"
     />
   )
 }
