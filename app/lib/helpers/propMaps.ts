da85369648947f5bec24b09adfc98575fe2e374a
Angelo Dias <oiangelodias@gmail.com>
2025-06-20 20:15:32 -0300

    lint

--- app/lib/helpers/propMaps.ts
+++ app/lib/helpers/propMaps.ts
@@ -26,6 +26,7 @@ export const eventPropNameMap = (property: keyof Event) => {
     title: "Nome",
     total_spots: "Lotação",
     is_applied: "Inscrite",
+    is_set_reminder: "Lembrete ativado",
   }[property]
 }
 
