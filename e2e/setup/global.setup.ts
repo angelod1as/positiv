923b49bf238849a2e558578f00193b993f38912c
Angelo Dias <oiangelodias@gmail.com>
2025-06-20 23:51:20 -0300

    update e2e

--- e2e/setup/global.setup.ts
+++ e2e/setup/global.setup.ts
@@ -1,6 +1,6 @@
 import test from "@playwright/test"
 import { createMockCredentials } from "e2e/helpers/create-mock-credentials"
-import { participantUserFile } from "e2e/helpers/load-user"
+import { adminUserFile, participantUserFile } from "e2e/helpers/load-user"
 import pwLog from "e2e/helpers/log"
 import { BasicDataPOM } from "e2e/poms/account/basic-data.pom"
 import { GenderPronounOrientationPOM } from "e2e/poms/account/gender-pronouns-orientation.pom"
@@ -41,4 +41,38 @@ test("authenticate as participant", async ({ page }) => {
 
   // End of authentication steps.
   await page.context().storageState({ path: participantUserFile })
+})
+
+test("authenticate as admin", async ({ page }) => {
+  pwLog("authenticating as admin")
+
+  const { email, password } = createMockCredentials()
+  await createMockUser(email, password, { admin: true })
+
+  const homepage = new HomepagePOM(page)
+  await homepage.goto()
+  await homepage.testBasicElements()
+  await homepage.testLoggedOut()
+
+  const loginPage = new LoginPOM(page)
+  await loginPage.goto()
+  await loginPage.testBasicElements()
+  await loginPage.testInvalidPassword()
+  await loginPage.testWrongCredentials()
+  await loginPage.doStraightLogin(email, password)
+
+  const termsPage = new AgreeToTermsPOM(page)
+  await termsPage.testAgreeToTerms()
+
+  const basicDataPage = new BasicDataPOM(page)
+  await basicDataPage.testFillBasicData()
+
+  const genderPage = new GenderPronounOrientationPOM(page)
+  await genderPage.testFillBasicData()
+
+  await homepage.goto()
+  await homepage.testAdminLoggedIn()
+
+  // End of authentication steps.
+  await page.context().storageState({ path: adminUserFile })
 })
