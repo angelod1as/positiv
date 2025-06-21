06c4d8d22dc35fdd318baa53ed60de715354fa80
Angelo Dias <oiangelodias@gmail.com>
2025-06-21 00:29:16 -0300

    add create mock event reminders

--- e2e/tests/flows/remind-me.spec.ts
+++ e2e/tests/flows/remind-me.spec.ts
@@ -7,10 +7,13 @@ import {
 import { AdminDashboardPOM } from "e2e/poms/admin/admin-dashboard-page.pom"
 import { DashboardPOM } from "e2e/poms/dashboard/dashboard.pom"
 import { MailhogPOM } from "e2e/poms/mailhog/mailhog.pom"
+import { createMockEventReminders } from "e2e/setup/create-mock-event-reminders"
 
 loadUser("admin")
 loadUser("participant")
 test("Apply to event", async ({ browser }) => {
+  await createMockEventReminders()
+
   const participantContext = await getParticipantContext(browser)
   const participantPage = await participantContext.newPage()
   const adminContext = await getAdminContext(browser)
@@ -20,7 +23,6 @@ test("Apply to event", async ({ browser }) => {
   await participantDashboard.goto()
   await participantDashboard.testRemindMeFunction()
 
-  // TODO: Admin test email
   const adminDashboard = new AdminDashboardPOM(adminPage)
   await adminDashboard.goto()
   await adminDashboard.testBasicElements()
