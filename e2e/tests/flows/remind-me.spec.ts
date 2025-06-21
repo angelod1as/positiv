9bf176a5203165068f03766c2c4f364a940d253b
Angelo Dias <oiangelodias@gmail.com>
2025-06-20 23:51:20 -0300

    update e2e

--- e2e/tests/flows/remind-me.spec.ts
+++ e2e/tests/flows/remind-me.spec.ts
@@ -1,23 +1,35 @@
 import { test } from "@playwright/test"
-import { getParticipantContext, loadUser } from "e2e/helpers/load-user"
+import {
+  getAdminContext,
+  getParticipantContext,
+  loadUser,
+} from "e2e/helpers/load-user"
+import { AdminDashboardPOM } from "e2e/poms/admin/admin-dashboard-page.pom"
 import { DashboardPOM } from "e2e/poms/dashboard/dashboard.pom"
+import { MailhogPOM } from "e2e/poms/mailhog/mailhog.pom"
 
+loadUser("admin")
 loadUser("participant")
 test("Apply to event", async ({ browser }) => {
   const participantContext = await getParticipantContext(browser)
   const participantPage = await participantContext.newPage()
-  // const adminContext = await getAdminContext(browser)
-  // const adminPage = await adminContext.newPage()
+  const adminContext = await getAdminContext(browser)
+  const adminPage = await adminContext.newPage()
 
   const participantDashboard = new DashboardPOM(participantPage)
   await participantDashboard.goto()
   await participantDashboard.testRemindMeFunction()
 
   // TODO: Admin test email
-  // const mailHogPage = new MailhogPOM(await page.context().newPage())
-  // await mailHogPage.goto()
-  // await mailHogPage.testBasicElements()
-  // await mailHogPage.testApplicationEmail()
+  const adminDashboard = new AdminDashboardPOM(adminPage)
+  await adminDashboard.goto()
+  await adminDashboard.testBasicElements()
+  await adminDashboard.testRemindMeEmails()
+
+  const mailHogPage = new MailhogPOM(await adminPage.context().newPage())
+  await mailHogPage.goto()
+  await mailHogPage.testBasicElements()
+  await mailHogPage.testReminderEmail()
 
   await participantDashboard.goto()
   await participantDashboard.testCancelRemindMeFunction()
