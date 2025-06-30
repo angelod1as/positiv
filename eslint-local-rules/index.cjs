1f078c176dfadec788074caf1f92e24c5c29ec9e
Angelo Dias <oiangelodias@gmail.com>
2025-06-29 21:39:20 -0300

    Add custom no-admin-imports

---
+++ eslint-local-rules/index.cjs
@@ -0,0 +1,15 @@
+// eslint-local-rules/index.cjs
+
+/**
+ * @fileoverview Disallows imports from the admin folder outside of the admin folder.
+ * @author Your Name
+ */
+
+"use strict"
+
+module.exports = {
+  // Define the rules provided by this plugin
+  rules: {
+    "no-admin-imports": require("./rules/no-admin-imports.cjs"),
+  },
+}
