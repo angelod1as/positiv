c671ddbc3f4b79816e4e13e9156ff31c81f5d073
Angelo Dias <oiangelodias@gmail.com>
2025-07-08 13:22:40 -0300

    add security migrations

---
+++ supabase/migrations/20250708131500_fix_function_search_path.sql
@@ -0,0 +1,6 @@
+-- Migration to fix mutable search_path for security functions
+
+ALTER FUNCTION public.add_user_role(uuid, text) SET search_path = 'public';
+
+-- ROO: REVIEW
+ALTER FUNCTION public.update_veteran_status() SET search_path = 'public';
