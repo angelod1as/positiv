2fe0854ae5682b7d62f0a36469c43eaf96d103b4
Angelo Dias <oiangelodias@gmail.com>
2025-07-11 10:48:56 -0300

    Fix spreadsheet

---
+++ app/lib/helpers/get-filtered-participants.ts
@@ -0,0 +1,34 @@
+import type { ProfileWithExtraData } from "~/business/admin/admin.server"
+import {
+  type ParticipantApplicationStatus,
+  type ParticipantAttendanceStatus,
+  type ProfileApprovedToAttendStatus,
+} from "~types/entities.types"
+
+export const getWillGoToEventParticipants = (
+  participants: ProfileWithExtraData[],
+) => {
+  const willGo = participants.filter((participant) => {
+    const notAttendance: ParticipantAttendanceStatus[] = [
+      "skipped",
+      "will-not-go",
+    ]
+    const notApplication: ParticipantApplicationStatus[] = [
+      "pending",
+      "talking",
+      "think_better",
+    ]
+    const notApproved: ProfileApprovedToAttendStatus[] = ["pending", "rejected"]
+    if (
+      notAttendance.includes(participant.attendance_status) ||
+      notApplication.includes(participant.application_status) ||
+      notApproved.includes(participant.approved_to_attend)
+    ) {
+      return false
+    }
+
+    return true
+  })
+
+  return { participants: willGo, count: willGo.length }
+}
