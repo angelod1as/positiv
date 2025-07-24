73bb0736c4b60ae8ef9d12059adc90cdff9b7410
Angelo Dias <oiangelodias@gmail.com>
2025-07-24 15:56:20 -0300

    test: update NEWS_VERSION in test to match constant

--- app/components/organisms/news-dialog/news-dialog.test.tsx
+++ app/components/organisms/news-dialog/news-dialog.test.tsx
@@ -199,7 +199,7 @@ describe('NewsDialog', () => {
       
       expect(mockSubmit).toHaveBeenCalledWith(
         {
-          newsVersion: '123456789',
+          newsVersion: '1753374084619',
           intent: 'news-update',
           thisUrl: 'http://localhost:3000/test',
         },
