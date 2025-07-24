b81700b48a5d91dafe5b03a4ef2d57aa9d663cb7
Angelo Dias <oiangelodias@gmail.com>
2025-07-24 16:03:51 -0300

    refactor(types): organize form schemas and component types

---
+++ app/types/forms/index.ts
@@ -0,0 +1,4 @@
+// Central export point for all form schemas
+export * from "./auth-schemas"
+export * from "./admin-schemas" 
+export * from "./participant-schemas"
---
+++ app/types/forms/participant-schemas.ts
@@ -0,0 +1,2 @@
+// Re-export participant-related schemas
+export { bdsmConsentSchema } from "~/business/participant/bdsm-consent"
