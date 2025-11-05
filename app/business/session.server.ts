2fc285de7c19daf4209d6321c28ac5b5de07a69c
Angelo Dias <oiangelodias@gmail.com>
2025-11-04 21:43:04 -0300

    feat(newsletter): add session cookie for modal dismissal

--- app/business/session.server.ts
+++ app/business/session.server.ts
@@ -29,3 +29,5 @@ export const rulesSessionStorage = createCookieSessionStorage<
 export const newsCookie = createCookie("show-news", {
   maxAge: 34560000, // max value
 })
+
+export const newsletterModalCookie = createCookie("newsletter-modal")
