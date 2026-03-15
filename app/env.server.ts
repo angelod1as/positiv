6f550e48da82f58ce26e7ec959e1dc5682754435
Angelo Dias <oiangelodias@gmail.com>
2026-03-15 10:03:22 -0300

    Revert "Merge pull request #546 from angelod1as/pos-289-payment-adicionar-variaveis-de-ambiente-do-asaas"
    This reverts commit 576c2938c5532b87cb3207e4abaa66ec03de665a, reversing
    changes made to c4472e7b4cae2bbe540d9fea5846da41de502112.

--- app/env.server.ts
+++ app/env.server.ts
@@ -47,11 +47,6 @@ const getEnvironment = makeTypedEnvironment(
 
     COOKIE_SECRET: zod.string().optional(),
 
-    ASAAS_API_KEY: zod.string().optional(),
-    ASAAS_WEBHOOK_TOKEN: zod.string().optional(),
-    ASAAS_ENVIRONMENT: zod
-      .enum(["sandbox", "production"])
-      .default("sandbox"),
     APP_URL: zod.string().optional(),
 
     TELEGRAM_BOT_TOKEN: zod.string().optional(),
