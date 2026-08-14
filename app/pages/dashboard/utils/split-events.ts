da7c9958627a5e9226848d07d74e83ec9c3a701d
Angelo Dias <oiangelodias@gmail.com>
2026-08-14 20:44:15 -0300

    feat(dashboard): split events by application instead of status

--- app/pages/dashboard/utils/split-events.ts
+++ app/pages/dashboard/utils/split-events.ts
@@ -1,27 +1,21 @@
-import { checkEventStatus } from "~/lib/helpers/check-event-status"
 import type { Event } from "~types/database/entities.types"
 
 export const splitEvents = (events: Event[] | undefined) => {
   const empty: {
-    registrationOpen: Event[]
-    scheduled: Event[]
-    registrationClosed: Event[]
+    applied: Event[]
+    available: Event[]
   } = {
-    registrationOpen: [],
-    scheduled: [],
-    registrationClosed: [],
+    applied: [],
+    available: [],
   }
 
   if (!events || events.length < 1) return empty
 
   return events.reduce((acc, event) => {
-    const { isOpen, isClosed } = checkEventStatus(event.event_status)
-    if (isOpen) {
-      acc.registrationOpen.push(event)
-    } else if (isClosed) {
-      acc.registrationClosed.push(event)
+    if (event.is_applied) {
+      acc.applied.push(event)
     } else {
-      acc.scheduled.push(event)
+      acc.available.push(event)
     }
     return acc
   }, empty)
